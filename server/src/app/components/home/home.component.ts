d6f14d78c681c6f472fdb0d0196f8b4ec9e0485f
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-06 18:29:00 -0400

    se obtiene el dispositivo seleccionado
    Se seleciona el componente dispositivo y se almacena en la variable
    dispositivo en Home

--- server/src/app/components/home/home.component.ts
+++ server/src/app/components/home/home.component.ts
@@ -8,31 +8,16 @@ import { FirebaseService } from '../../service/firebase.service';
 })
 export class HomeComponent {
 
-  data:any[] = [];
-  valuesArray:any[] = [];
-  paramValue:any[] = [];
+  dispositivo:string = "";
+  parametro:string = "";
 
   constructor(private _firebaseService:FirebaseService) {
-    this._firebaseService.getData().subscribe( data => {
-      // Recorrer el JSON que en cada llave contiene un conjunto de datos
-      let x:number = 0;
-      for( let objeto of Object.keys(data) ){
-        this.data[x] = data[objeto];
-        let i:number = 0;
-        for (let param of Object.keys(data[objeto].values)){
-          // extraer el parametro y valor para agregarlos en valuesArray, una vez agregados, limpiar el valuesArray
-          this.paramValue[0] = param;
-          this.paramValue[1] = parseFloat(data[objeto].values[param]);
-          this.valuesArray[i] = this.paramValue;
-          this.paramValue = [];
-          i++;
-        }
-        this.data[x].values = this.valuesArray;
-        this.valuesArray = [];
-         x++;
-      }
-      console.log(this.data);
-    });
+
+  }
+
+  setDispositivo(dispositivo:string){
+    this.dispositivo = dispositivo;
+    console.log(this.dispositivo);
   }
 
 }
