40c8c5da5bce743f1085a77c1c8d5c4745adc2fd
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-06 18:29:27 -0400

    Se obtiene la data en el constructor de dispositivos y data

--- server/src/app/service/firebase.service.ts
+++ server/src/app/service/firebase.service.ts
@@ -1,18 +1,49 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/Rx';
 
 @Injectable()
 export class FirebaseService {
 
-  url:string = "https://libelium-91af3.firebaseio.com/";
+  dataFiresbase:any;
+  valuesArray:any[] = [];
+  paramValue:any[] = [];
 
-  constructor(private http:Http) { }
+  data:any[] = [];
+  dispositivos:string[] = [];
 
-  getData(){
-    let urlDisp = `${ this.url }estacionMetereologica.json?orderBy"timestamp"`;
-    return this.http.get( urlDisp ).map( res => {
-      return res.json();
+  urlEM:string = "https://libelium-91af3.firebaseio.com/";
+
+  constructor(private http:HttpClient) {
+    // obtener dispositivos
+    let urlDevices = `${ this.urlEM }dispositivos.json`;
+    this.http.get( urlDevices ).subscribe( res => {
+      for(let dispositivo of Object.keys(res)){
+        this.dispositivos.push(dispositivo);
+      }
+    });
+
+    let urlData = `${ this.urlEM }estacionMetereologica.json?orderBy"timestamp"`;
+    this.http.get( urlData ).subscribe( res => {
+      this.dataFiresbase = res;
+      // Recorrer el JSON que en cada llave contiene un conjunto de datos
+      let x:number = 0
+      for( let objeto of Object.keys(this.dataFiresbase) ){
+         this.data[x] = this.dataFiresbase[objeto];
+         let i:number = 0;
+         for (let param of Object.keys(this.dataFiresbase[objeto].values)){
+           // extraer el parametro y valor para agregarlos en valuesArray, una vez agregados, limpiar el valuesArray
+           this.paramValue[0] = param;
+           this.paramValue[1] = parseFloat(this.dataFiresbase[objeto].values[param]);
+           this.valuesArray[i] = this.paramValue;
+           this.paramValue = [];
+           i++;
+         }
+         this.data[x].values = this.valuesArray;
+         this.valuesArray = [];
+         x++;
+       }
     })
   }
+
 }
