302f07d7ea0d5b13375d5849994c8f28c27956a4
marianssa <mariajoselaylay@gmail.com>
2018-06-06 14:54:02 -0400

    Envio de datos
    los datos se envian, pero existe un problema con el tipo de dato

--- Arduino/XBee/Envio de datos/envio_de_datos/envio_de_datos.ino
+++ Arduino/XBee/Envio de datos/envio_de_datos/envio_de_datos.ino
@@ -1,4 +1,3 @@
-#include <Printers.h>
 #include <XBee.h>
 
 //Objeto XBee
@@ -6,21 +5,26 @@
 XBee xbee = XBee();
 
 // SH + SL Address of receiving XBee
-uint8_t payload[] = {0,0 };
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40dc588f);
+
+uint8_t payload[] = {0,0};
 ZBTxRequest mensaje = ZBTxRequest(addr64, payload, sizeof(payload));
 
 
 void setup() {
-  delay(1000);
+
   Serial.begin(9600);
   xbee.setSerial(Serial);
-  xbee.send(mensaje);
+  
 
 }
 
 void loop() {
 
+payload[0] = 2 & 0xff;;
+payload[1]= 5 & 0xff;
+xbee.send(mensaje);
+delay(1000);
  }
 
 
