b3bcaa2d02e4b98b9c7a75a4d16917704c6e410e
marianssa <mariajoselaylay@gmail.com>
2018-06-20 11:25:37 -0400

    Envio de datos 2

--- Arduino/XBee/Envio de datos/envio_de_datos/envio_de_datos.ino
+++ Arduino/XBee/Envio de datos/envio_de_datos/envio_de_datos.ino
@@ -1,93 +1,102 @@
-#include <Printers.h>
+//#include <avr/dtostrf.h>
+#include <stdlib.h>
 #include <XBee.h>
 //Objetos
 
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
+
 // SH + SL Address of receiving XBee
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40dc588f);
-uint8_t payload[]={0,0};
-ZBTxRequest mensaje= ZBTxRequest(addr64, payload, sizeof(payload));
+char* payload= "";
+ZBTxRequest mensaje = ZBTxRequest(addr64, (uint8_t*)payload, sizeof(payload));
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
-//conversion de float a bytes
-//union u_tag {
-//    uint8_t b[4];
-//    float fval;
-//} u;
-//float type is 32bits (4 bytes) length
-char FloatToBytes(float valor)
+union {
+  float f;
+  byte b[4];
+} valor;
+
+union FloatChar {
+  float f;
+  char c[sizeof(float)];
+};
+//char FloatToBytes(float valor)
+//{
+//   char b[sizeof(valor)];
+//   memcpy(b, &valor, sizeof(valor));
+//   return b;
+//}
+
+char* aux;
+char aux2[10];
+String mssg = "V ";
+char resultado[50];
+void setup()
 {
-   char b[sizeof(valor)];
-   memcpy(b, &valor, sizeof(valor));
-   return b;
-}
-
-
-char aux;
-String aux2;
-void setup() {
 
   Serial.begin(9600);
-    xbee.setSerial(Serial);
-  
-
+  xbee.setSerial(Serial);
 }
 
-void loop() {
-//lectura de sensores ejemplo
- float humedad = 5;
- float temperatura = 10;
+void loop()
+{
+  //lectura de sensores ejemplo
+  float humedad = 5.0;
+  float temperatura = 10;
   //chequeo si son valores nulos
- if (!isnan(temperatura) && !isnan(humedad)) 
- {
-//     u.fval = humedad;
-//     payload[0] = u.b; 
-//   u.fval = temperatura;
-//   payload[1] = u.b;
-//    aux2 = String(humedad);
-//    aux2.toCharArray(aux,1);
-//    payload[0] = aux & 0xff;
-      aux = humedad;
-      payload[0] = 'temperatura'; 
-      payload[1]= aux; //& 0xff;
-//m;
-
-//     aux = FloatToBytes(humedad);
-//     payload[0]= aux;
- }
-xbee.send(mensaje);
-//luego de enviar se espera una respuesta "response"
-if (xbee.readPacket(500)) 
-{
-// se obtuvo respuesta            
-if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) 
-{
-  xbee.getResponse().getZBTxStatusResponse(txStatus);
-
-  // estado del envio
-  if (txStatus.getDeliveryStatus() == SUCCESS) 
+  if (!isnan(temperatura) && !isnan(humedad))
   {
+//    aux2 = "H ";
+    dtostrf(humedad, 4, 2, resultado);
+////    aux = resultado;
+//    FloatChar fc;
+//    fc.f = humedad;
+//    strcpy(resultado, fc.c);
+//    humedad += 1.01;
+//    strcat(payload, aux2);
+//    strcat(payload, resultado);
+      //mssg= mssg + "H : " + humedad;
+      //mssg.toCharArray(resultado,50);
+      Serial.println(resultado);
+      strcpy(payload, resultado);
 
-    Serial.println("Logrado");
   }
-  else 
+
+  xbee.send(mensaje);
+  if (xbee.readPacket(500))
   {
-    //El destinatario no a recibido el mensaje
-    Serial.println("Mensaje perdido");
+    // se obtuvo respuesta
+    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE)
+    {
+      xbee.getResponse().getZBTxStatusResponse(txStatus);
+
+      // estado del envio
+      if (txStatus.getDeliveryStatus() == SUCCESS)
+      {
+
+        Serial.println("Logrado");
+      }
+      else
+      {
+        //El destinatario no a recibido el mensaje
+        Serial.println("Mensaje perdido");
+      }
+    }
+
   }
-}
+  else if (xbee.getResponse().isError())
+  {
+    Serial.print("error al leer el paquete, codigo: ");
+    Serial.println(xbee.getResponse().getErrorCode());
+  }
+  else
+  {
+    Serial.println("Esto no deberia pasar");
+  }
 
-} 
-else if (xbee.getResponse().isError()) 
-{
-Serial.print("error al leer el paquete, codigo: ");
-Serial.println(xbee.getResponse().getErrorCode());
-}
-else 
-{
-Serial.println("Esto no deberia pasar");
-}
-
-delay(5000);
+  strcpy(payload, "");
+  strcpy(resultado, "");
+  mssg = "V";
+  delay(5000);
 }
