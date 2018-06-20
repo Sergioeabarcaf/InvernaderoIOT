55214534f3b2f12ea5f93cab6dfa5c13aea2099a
marianssa <mariajoselaylay@gmail.com>
2018-06-20 12:44:09 -0400

    Correcion envio de mensajes

--- Arduino/XBee/Envio de datos/envio_de_datos/envio_de_datos.ino
+++ Arduino/XBee/Envio de datos/envio_de_datos/envio_de_datos.ino
@@ -8,30 +8,20 @@ XBeeResponse response = XBeeResponse();
 
 // SH + SL Address of receiving XBee
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40dc588f);
-char* payload= "";
-ZBTxRequest mensaje = ZBTxRequest(addr64, (uint8_t*)payload, sizeof(payload));
+//variable mensaje
+char payload[50];
+//estado del envio
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
-union {
-  float f;
-  byte b[4];
-} valor;
+void floatAchar(float num, char resultado[10])
+{
+    
+    dtostrf(num,5,3,resultado);
+    
+}
 
-union FloatChar {
-  float f;
-  char c[sizeof(float)];
-};
-//char FloatToBytes(float valor)
-//{
-//   char b[sizeof(valor)];
-//   memcpy(b, &valor, sizeof(valor));
-//   return b;
-//}
 
-char* aux;
-char aux2[10];
-String mssg = "V ";
-char resultado[50];
+char aux[10];
 void setup()
 {
 
@@ -42,28 +32,27 @@ void setup()
 void loop()
 {
   //lectura de sensores ejemplo
-  float humedad = 5.0;
-  float temperatura = 10;
+  float humedad = 5.15;
+  float temperatura = 10.25;
   //chequeo si son valores nulos
   if (!isnan(temperatura) && !isnan(humedad))
   {
-//    aux2 = "H ";
-    dtostrf(humedad, 4, 2, resultado);
-////    aux = resultado;
-//    FloatChar fc;
-//    fc.f = humedad;
-//    strcpy(resultado, fc.c);
-//    humedad += 1.01;
-//    strcat(payload, aux2);
-//    strcat(payload, resultado);
-      //mssg= mssg + "H : " + humedad;
-      //mssg.toCharArray(resultado,50);
-      Serial.println(resultado);
-      strcpy(payload, resultado);
-
+    //dtostrf(variable,mumero de digitos,numero de decimales,matriz);
+    floatAchar(humedad,aux);
+     strcpy(payload,"H :");
+     strcat(payload,aux);
+     strcat(payload,"#");
+    floatAchar(temperatura,aux);
+     strcat(payload,"T :");
+     strcat(payload,aux);
+     strcat(payload,"#");
+    //Serial.println(payload);
+ //   Serial.println(resultado);
   }
 
-  xbee.send(mensaje);
+
+ZBTxRequest mensaje = ZBTxRequest(addr64, (uint8_t*)payload, sizeof(payload));
+xbee.send(mensaje);
   if (xbee.readPacket(500))
   {
     // se obtuvo respuesta
@@ -94,9 +83,9 @@ void loop()
   {
     Serial.println("Esto no deberia pasar");
   }
-
-  strcpy(payload, "");
-  strcpy(resultado, "");
-  mssg = "V";
-  delay(5000);
+  strcpy(payload,"");
+  
+   delay(5000);
 }
+
+
