aa6831f1288f42c85c4a2e945c0e8f179ca88320
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-21 17:21:38 -0400

    creacion componenter history

--- server/src/app/app.routes.ts
+++ server/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HistoryComponent } from './components/history/history.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'history', component: HistoryComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
