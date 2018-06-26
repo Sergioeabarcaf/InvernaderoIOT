c4c9cdd4bc53de2117910f50554f4e5271d74099
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-26 13:35:12 -0400

    base de dashboard historico con 24 ultimas horas

--- server/src/app/components/history/history.component.ts
+++ server/src/app/components/history/history.component.ts
@@ -11,8 +11,6 @@ export class HistoryComponent {
   // lineChart
  public lineChartData:Array<any> = [
    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-   {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-   {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
  ];
  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
  public lineChartOptions:any = {
@@ -44,7 +42,7 @@ export class HistoryComponent {
      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
    }
  ];
- public lineChartLegend:boolean = true;
+ public lineChartLegend:boolean = false;
  public lineChartType:string = 'line';
 
   constructor() { }
