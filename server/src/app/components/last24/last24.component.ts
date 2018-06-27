6dd4b967d0ec7001aad77660eea003b5a4c070b4
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-27 17:56:56 -0400

    traspaso de historico a last24

--- server/src/app/components/last24/last24.component.ts
+++ server/src/app/components/last24/last24.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-last24',
   templateUrl: './last24.component.html',
   styleUrls: ['./last24.component.css']
 })
-export class Last24Component implements OnInit {
+export class Last24Component {
+  @Input() parametro:string;
 
-  constructor() { }
+  //gauge
+  @Input() gaugeValue:number;
+  @Input() gaugeMin:number;
+  @Input() gaugeMax:number;
 
-  ngOnInit() {
+  //lineChart
+  @Input() lineChartData:Array<any>;
+  @Input() lineChartLabels:Array<any>;
+
+ public lineChartColors:Array<any> = [
+   {
+     backgroundColor: 'rgba(79,136,161,0.8)',
+     borderColor: 'rgb(200,123,37)',
+     pointBackgroundColor: 'rgba(148,159,177,1)',
+     pointBorderColor: '#fff',
+     pointHoverBackgroundColor: '#fff',
+     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+   }
+ ];
+ public lineChartType:string = 'line';
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
   }
 
 }
