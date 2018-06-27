8257bb6bf1ceb455fc1b9c60055a94c5d2d3708c
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-27 13:51:20 -0400

    creacion de modulo para filtros y para ultimas 24 horas de datos

---
+++ server/src/app/components/last24/last24.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-last24',
+  templateUrl: './last24.component.html',
+  styleUrls: ['./last24.component.css']
+})
+export class Last24Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
