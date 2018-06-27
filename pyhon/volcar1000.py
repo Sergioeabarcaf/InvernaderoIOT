a687377255ce05cade068cb5556c613e458ec768
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-27 18:42:52 -0400

    python para volcar el contenido de los id especificados y x cantidad

---
+++ pyhon/volcar1000.py
@@ -0,0 +1,22 @@
+import goToFirebase
+import drm
+import json
+import base64
+
+id = ["00:13:A2:00:41:5B:67:EB","00:13:A2:00:41:04:37:F1"]
+cantidad = 50
+
+#Recorrer el arreglo con id
+for disp in id:
+    # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
+    data = json.loads(drm.getDataDevice(disp,cantidad))
+    for x in data['list']:
+        dataValue = base64.b64decode(x['value'])
+        dataTime = x['timestamp']
+        # Limpiar la data y obtener el dispositivo con sus valores
+        device, data = drm.obtenerData(dataValue)
+        if goToFirebase.checkData(dataTime, data[0]):
+            goToFirebase.send(dataTime,disp,data)
+            goToFirebase.updateLast(str(dataTime), data)
+        else:
+            print "El dato ya habia sido cargado"
