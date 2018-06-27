4faf6d9d2885e14054915fc2f3d6719fb0ba4432
sergioeabarcaf <sergioaf1991@gmail.com>
2018-06-27 17:58:45 -0400

    uso de componente hijos y uso de fireService

--- server/src/app/components/history/history.component.ts
+++ server/src/app/components/history/history.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FirebaseService } from '../../service/firebase.service';
 
 @Component({
   selector: 'app-history',
@@ -8,68 +8,22 @@ import { NgForm } from '@angular/forms';
 })
 export class HistoryComponent {
 
-  // lineChart
- public lineChartData:Array<any> = [
-   {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
- ];
- public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
- public lineChartOptions:any = {
-   responsive: true
- };
- public lineChartColors:Array<any> = [
-   { // grey
-     backgroundColor: 'rgba(148,159,177,0.2)',
-     borderColor: 'rgba(148,159,177,1)',
-     pointBackgroundColor: 'rgba(148,159,177,1)',
-     pointBorderColor: '#fff',
-     pointHoverBackgroundColor: '#fff',
-     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-   },
-   { // dark grey
-     backgroundColor: 'rgba(77,83,96,0.2)',
-     borderColor: 'rgba(77,83,96,1)',
-     pointBackgroundColor: 'rgba(77,83,96,1)',
-     pointBorderColor: '#fff',
-     pointHoverBackgroundColor: '#fff',
-     pointHoverBorderColor: 'rgba(77,83,96,1)'
-   },
-   { // grey
-     backgroundColor: 'rgba(148,159,177,0.2)',
-     borderColor: 'rgba(148,159,177,1)',
-     pointBackgroundColor: 'rgba(148,159,177,1)',
-     pointBorderColor: '#fff',
-     pointHoverBackgroundColor: '#fff',
-     pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-   }
- ];
- public lineChartLegend:boolean = false;
- public lineChartType:string = 'line';
+  paramTemp:string = "Temperatura";
+  paramHum:string = "Humedad";
+  paramPres:string = "Presión";
+  paramCO2:string = "Dioxido de carbono";
+  gaugeMinTemp:number=-20;
+  gaugeMinHum:number=0;
+  gaugeMinPres:number=0;
+  gaugeMinCo2:number=0;
+  gaugeMaxTemp:number=100;
+  gaugeMaxHum:number=100;
+  gaugeMaxPres:number=110000;
+  gaugeMaxCo2:number=2000;
 
-  constructor() { }
+  constructor(public _firebaseService:FirebaseService) {
 
-  verGrafico(forma:NgForm){
-    console.log(forma.controls.parametro.value);
-    console.log(forma);
   }
 
- public randomize():void {
-   let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-   for (let i = 0; i < this.lineChartData.length; i++) {
-     _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-     for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-       _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-     }
-   }
-   this.lineChartData = _lineChartData;
- }
-
- // events
- public chartClicked(e:any):void {
-   console.log(e);
- }
-
- public chartHovered(e:any):void {
-   console.log(e);
- }
 
 }
