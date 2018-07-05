ab742cc190a76fd3fdb00c21ecc49d6d11cd5608
marianssa <mariajoselaylay@gmail.com>
2018-07-05 15:37:50 -0400

    incluido : sensor humedad de suelo y t° de contacto

--- Arduino/XBee/envio_de_datos/envio_de_datos.ino
+++ Arduino/XBee/envio_de_datos/envio_de_datos.ino
@@ -1,16 +1,23 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 //#include <avr/dtostrf.h>
 #include <stdlib.h>
 #include <DHT.h>
 #include <XBee.h>
 
-#define pin 4
+#define pindht 4
+#define pinhc A0
+#define pintc 5
 #define DHTTYPE DHT11
 
 //---------Objetos
 
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
-DHT dht (pin, DHTTYPE);
+DHT dht (pindht, DHTTYPE);
+OneWire oneWireobjeto(pintc);
+DallasTemperature sensorDS18B20(&oneWireobjeto);
 
 //--------- SH + SL Address of receiving XBee
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40dc588f);
@@ -39,70 +46,86 @@ void setup()
   Serial.begin(9600);
   xbee.setSerial(Serial);
   dht.begin();
-
+  sensorDS18B20.begin();
 }
 
 void loop()
 {
+  // Se ajustan los valores del fc-28
   
-    //lectura de sensores ejemplo
-    float humedad = dht.readHumidity();
-    float temperatura = dht.readTemperature();
+  int humedad_suelo = analogRead(pinhc); 
+  float porcentaje = (100*humedad_suelo)/700;
+  //lectura temperatura de contacto DS18B20
+  sensorDS18B20.requestTemperatures();
 
-    //chequeo si son valores nulos
-    if (!isnan(temperatura) && !isnan(humedad))
+  //lectura de sensor dht11
+  float humedad = dht.readHumidity();
+  float temperatura = dht.readTemperature();
+
+  //chequeo si son valores nulos
+  if (!isnan(temperatura) && !isnan(humedad) && !isnan(humedad_suelo))
+  {
+    floatAchar(humedad, aux);
+    strcpy(payload, "H :");
+    strcat(payload, aux);
+    strcat(payload, "#");
+    floatAchar(temperatura, aux);
+    strcat(payload, "T :");
+    strcat(payload, aux);
+    strcat(payload, "#");
+    floatAchar(sensorDS18B20.getTempCByIndex(0), aux);
+    strcat(payload, "Tc :");
+    strcat(payload, aux);
+    strcat(payload, "#");
+    floatAchar(porcentaje, aux);
+    strcat(payload, "Hc :");
+    strcat(payload, aux);
+    strcat(payload, "#");
+
+    Serial.println(payload);
+
+  }
+
+  //Envio de mensaje-----------------------------------------------------------------------
+
+  ZBTxRequest mensaje = ZBTxRequest(addr64, (uint8_t*)payload, sizeof(payload));
+  xbee.send(mensaje);
+
+  if (xbee.readPacket(500))
+  {
+    // se obtuvo respuesta
+    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE)
     {
-      floatAchar(humedad, aux);
-      strcpy(payload, "H :");
-      strcat(payload, aux);
-      strcat(payload, "#");
-      floatAchar(temperatura, aux);
-      strcat(payload, "T :");
-      strcat(payload, aux);
-      strcat(payload, "#");
-      //Serial.println(payload);
-      //   Serial.println(resultado);
-    }
+      xbee.getResponse().getZBTxStatusResponse(txStatus);
 
-//Envio de mensaje-----------------------------------------------------------------------
-
-    ZBTxRequest mensaje = ZBTxRequest(addr64, (uint8_t*)payload, sizeof(payload));
-    xbee.send(mensaje);
-
-    if (xbee.readPacket(500))
-    {
-      // se obtuvo respuesta
-      if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE)
+      // estado del envio
+      if (txStatus.getDeliveryStatus() == SUCCESS)
       {
-        xbee.getResponse().getZBTxStatusResponse(txStatus);
 
-        // estado del envio
-        if (txStatus.getDeliveryStatus() == SUCCESS)
-        {
-
-          Serial.println("Logrado");
-        }
-        else
-        {
-          //El destinatario no a recibido el mensaje
-          Serial.println("Mensaje perdido");
-        }
+        Serial.println("Logrado");
       }
+      else
+      {
+        //El destinatario no a recibido el mensaje
+        Serial.println("Mensaje perdido");
+      }
+    }
 
-    }
-    else if (xbee.getResponse().isError())
-    {
-      Serial.print("error al leer el paquete, codigo: ");
-      Serial.println(xbee.getResponse().getErrorCode());
-    }
-    else
-    {
-      Serial.println("Esto no deberia pasar");
-    }
-    // ------------------------------------------ Limpieza mensaje
+  }
+  else if (xbee.getResponse().isError())
+  {
+    Serial.print("error al leer el paquete, codigo: ");
+    Serial.println(xbee.getResponse().getErrorCode());
+  }
+  else
+  {
+    Serial.println("Esto no deberia pasar");
+  }
+  // ------------------------------------------ Limpieza mensaje
 
-    strcpy(payload, "");
-    delay(1800000);    
- }
+  strcpy(payload, "");
+
+  delay(1800000);
+}
 
 
