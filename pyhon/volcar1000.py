1d08444cbaba175f4842d5f57373d11faab8e8e9
sergioeabarcaf <sergioaf1991@gmail.com>
2018-09-27 18:24:42 -0300

    Python - Cambio en ordenamiento de elementos

--- pyhon/volcar1000.py
+++ pyhon/volcar1000.py
@@ -4,13 +4,14 @@ import json
 import base64
 
 id = ["00:13:A2:00:41:5B:67:EB","00:13:A2:00:41:04:37:F1"]
-cantidad = 50
+cantidad = 60
 
 #Recorrer el arreglo con id
 for disp in id:
     # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
     data = json.loads(drm.getDataDevice(disp,cantidad))
-    for x in data['list']:
+    dataReverse = data['list'][::-1]
+    for x in dataReverse:
         dataValue = base64.b64decode(x['value'])
         dataTime = x['timestamp']
         # Limpiar la data y obtener el dispositivo con sus valores
