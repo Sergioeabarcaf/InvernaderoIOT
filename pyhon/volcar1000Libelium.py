60559d6fd3a399535b13edcbe0ea5f4ed25b2c0c
sergioeabarcaf <sergioaf1991@gmail.com>
2018-10-03 12:11:42 -0300

    modificacion de archivo volcar1000 y renombrado

--- pyhon/volcar1000Libelium.py
+++ pyhon/volcar1000Libelium.py
@@ -3,21 +3,19 @@ import drm
 import json
 import base64
 
-id = ["00:13:A2:00:41:5B:67:EB","00:13:A2:00:41:04:37:F1"]
-cantidad = 60
+id = ["00:13:A2:00:41:04:37:F1","00:13:A2:00:41:5B:67:EB"]
+cantidad = 1000
 
 #Recorrer el arreglo con id
 for disp in id:
     # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
     data = json.loads(drm.getDataDevice(disp,cantidad))
+    # ordenar dejando el dato mas nuevo al final del array
     dataReverse = data['list'][::-1]
     for x in dataReverse:
         dataValue = base64.b64decode(x['value'])
         dataTime = x['timestamp']
         # Limpiar la data y obtener el dispositivo con sus valores
         device, data = drm.obtenerData(dataValue)
-        if goToFirebase.checkData(dataTime, data[0]):
+        if goToFirebase.checkData(dataTime, disp):
             goToFirebase.send(dataTime,disp,data)
-            goToFirebase.updateLast(str(dataTime), data)
-        else:
-            print "El dato ya habia sido cargado"
