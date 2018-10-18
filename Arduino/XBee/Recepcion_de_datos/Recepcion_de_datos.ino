580f647796a57acb9f80c31583e28ff01eaae2c3
marianssa <mariajoselaylay@gmail.com>
2018-10-18 16:13:29 -0300

    Envio de mensaje de prueba - Recepcion en xbee
    Recepcion en xbee falta identificar que parte del paquete recibido corresponde a los datos y luego convertirlo

--- Arduino/XBee/Recepcion_de_datos/Recepcion_de_datos.ino
+++ Arduino/XBee/Recepcion_de_datos/Recepcion_de_datos.ino
@@ -2,11 +2,12 @@
 //---------------------------------------
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
-Rx64Response rx64 = Rx64Response();
-uint8_t data;
+
+char data[15];
 uint8_t posicionDatos;
 String auxStr;
-
+int auxInt;
+Rx16Response rx16 = Rx16Response();
 
 //funcion para convertir datos uint8_t a String
 String toString(uint8_t *str){
@@ -17,53 +18,44 @@ String toString(uint8_t *str){
 void setup() 
 {
   Serial.begin(9600);
+  xbee.setSerial(Serial);
 }
 
 void loop()
 {
 //se espera la llegada de un paquete durante 100milliseg antes de continuar
-  xbee.readPacket(100) ;
-  
+  xbee.readPacket(500) ;
   if (xbee.getResponse().isAvailable()) {
     //Se leyo algo
-        if (xbee.getResponse().getApiId() == RX_64_RESPONSE) {
-                xbee.getResponse().getRx64Response(rx64);
-                Serial.print(F("Paquete Recibido de: "));
-                rx64.getRemoteAddress64();
-                Serial.print(F("Señal: "));
-                rx64.getRssi();
-                posicionDatos = rx64.getDataOffset();
-                posicionDatos = atoi(posicionDatos);
-                Serial.print("Posicion-");
-                Serial.print(posicionDatos);
-                data=rx64.getData();
-                Serial.write(rx64.getData(),rx64.getDataLength());
-                Serial.println();
-                Serial.println("Data-");
-                Serial.println(data);    
-                //particion de String
-                auxStr = toString(data);
-                Serial.print(auxStr);
-                auxStr=auxStr.substring(posicionDatos);
-                Serial.print( auxStr);
-
-                for(int i = 0; i<xbee.getResponse().getFrameDataLength();i++)
-                {
-                  Serial.print(xbee.getResponse().getFrameData()[i],HEX);
-                  Serial.print(" ");
-                }
-                
-        }       
-}
-
-else if(xbee.getResponse().isError()){
- /*-------------------
-  1 - CHECKSUM_FAILURE
-  2 - PACKET_EXCEEDS_BYTE_ARRAY_LENGTH
-  3 - UNEXPECTED_START_BYTE*/
-  Serial.print("Codigo de Error: ");
-  Serial.print(xbee.getResponse().getErrorCode(),DEC);
-}
+    Serial.println("LLego Mensaje");
+    xbee.getResponse().getRx16Response(rx16);
+    rx16.getRemoteAddress16();
+    Serial.println(F("Señal: "));
+    rx16.getRssi();
+    Serial.print("Mensaje:");
+    for (int i = 0; i < rx16.getDataLength(); i++) 
+    { 
+      Serial.print(rx16.getData(i),HEX); 
+      auxStr= auxStr + String(rx16.getData(i),HEX);
+    }
+    Serial.println(""); 
+    Serial.print("Mensaje:");
+    Serial.println(auxStr);
+    posicionDatos = rx16.getDataOffset();
+    Serial.println("Posicion Datos:");
+    auxInt=atoi(posicionDatos);
+    Serial.print(auxInt);
+    
+  }
+    
+  else if(xbee.getResponse().isError()){
+   /*-------------------
+    1 - CHECKSUM_FAILURE
+    2 - PACKET_EXCEEDS_BYTE_ARRAY_LENGTH
+    3 - UNEXPECTED_START_BYTE*/
+    Serial.print("Codigo de Error: ");
+    Serial.print(xbee.getResponse().getErrorCode(),DEC);
+  }
                
 
 }
