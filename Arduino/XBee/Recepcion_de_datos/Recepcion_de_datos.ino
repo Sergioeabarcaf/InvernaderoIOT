c00e33a30a0d7d882ae70a80f9323133a9a7453f
marianssa <mariajoselaylay@gmail.com>
2018-10-24 13:59:00 -0300

    Recepcion de datos actualizado
    Se envia un mensaje con un # y se divide el paquete en el punto del #

--- Arduino/XBee/Recepcion_de_datos/Recepcion_de_datos.ino
+++ Arduino/XBee/Recepcion_de_datos/Recepcion_de_datos.ino
@@ -1,4 +1,6 @@
 #include <XBee.h> 
+#include <string.h>
+
 //---------------------------------------
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
@@ -14,6 +16,56 @@ String toString(uint8_t *str){
     return String((char *)str);
 }
 
+//Extrae el mensaje del frame recibido
+String ObtenerDataFrame(String Frame, int Framelength)
+{
+  //busca en el string el caracter # = 23 (hex)
+  String strAux = "23";  
+  String strAuxmensaje;
+  int indice;
+  indice = Frame.indexOf(strAux,0);
+  strAuxmensaje = Frame.substring(indice);
+  return strAuxmensaje;
+}
+
+int getDigitValue(char digit) {
+  int n = digit-'0';
+  if (n>=0 && n<10) { return n; }
+  if (digit=='A') { return 10; }
+  if (digit=='B') { return 11; }
+  if (digit=='C') { return 12; }
+  if (digit=='D') { return 13; }
+  if (digit=='E') { return 14; }
+  if (digit=='F') { return 15; }
+  return -1;
+}
+
+int getPairValue(char digit1, char digit2) {
+  int v1 = getDigitValue(digit1);
+  int v2 = getDigitValue(digit2);
+  return (v1==-1 || v2==-1) ? -1 : v1 * 16 + v2;
+}
+
+bool hexStrToStr(char *dst, const char *src, int size) {
+  bool isGood = false;
+  if (size % 2 == 0) {
+    isGood = true;
+    for (int i=0; i<size; i+=2) {
+      int pv = getPairValue(src[i], src[i+1]);
+      if (pv==-1) { isGood = false; break; }
+      *dst = pv;
+      dst++;
+    }
+  }
+  *dst = 0;
+  return isGood;
+}
+
+bool hexStrToStr(char *dst, const char *src) 
+{ 
+  return hexStrToStr(dst, src, strlen(src)); 
+}
+
 
 void setup() 
 {
@@ -23,6 +75,14 @@ void setup()
 
 void loop()
 {
+ 
+  //variable para almacenar solo la RF data del Frame
+  String datosFrame;
+  String auxString;
+  char strAscii(50);
+  char rfdata[50];
+  int longitud;
+  
 //se espera la llegada de un paquete durante 100milliseg antes de continuar
   xbee.readPacket(500) ;
   if (xbee.getResponse().isAvailable()) {
@@ -35,19 +95,18 @@ void loop()
     Serial.print("Mensaje:");
     for (int i = 0; i < rx16.getDataLength(); i++) 
     { 
-      Serial.print(rx16.getData(i),HEX); 
-      auxStr= auxStr + String(rx16.getData(i),HEX);
+      Serial.print(rx16.getData(i),DEC); 
+      auxString= auxString + String(rx16.getData(i),DEC);
     }
-    Serial.println(""); 
-    Serial.print("Mensaje:");
-    Serial.println(auxStr);
-    posicionDatos = rx16.getDataOffset();
-    Serial.println("Posicion Datos:");
-    auxInt=atoi(posicionDatos);
-    Serial.print(auxInt);
-    
+   longitud = rx16.getDataLength();
+   datosFrame = ObtenerDataFrame(auxString, longitud);
+   Serial.println("");
+   Serial.println("RF DATA :");
+   Serial.print(datosFrame);
+   datosFrame.toCharArray(rfdata,50);
+   Serial.println(hexStrToStr(strAscii,rfdata));
+   Serial.println(strAscii);
   }
-    
   else if(xbee.getResponse().isError()){
    /*-------------------
     1 - CHECKSUM_FAILURE
@@ -56,6 +115,7 @@ void loop()
     Serial.print("Codigo de Error: ");
     Serial.print(xbee.getResponse().getErrorCode(),DEC);
   }
-               
+
+  
 
 }
