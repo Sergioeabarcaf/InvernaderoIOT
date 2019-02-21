596c4dce836040fae00883620e1354be3d58eac4
sergioeabarcaf <sergioaf1991@gmail.com>
2019-02-21 18:57:49 +0000

    sleep 60 en funcion

--- pyhon/libelium30.py
+++ pyhon/libelium30.py
@@ -2,17 +2,20 @@ import goToFirebase
 import drm
 import json
 import base64
+import time
 
 id = ["00:13:A2:00:41:04:37:F1","00:13:A2:00:41:5B:67:EB"]
 cantidad = 1
 
-for disp in id:
-    # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
-    data = json.loads(drm.getDataDevice(disp,cantidad))
-    if data['list']:
-        dataValue = base64.b64decode(data['list'][0]['value'])
-        dataTime = data['list'][0]['timestamp']
-        # Limpiar la data y obtener el dispositivo con sus valores
-        device,data = drm.obtenerData(dataValue)
-        if goToFirebase.checkData(dataTime, disp):
-            goToFirebase.send(dataTime,disp,data)
+while (True):
+    for disp in id:
+        # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
+        data = json.loads(drm.getDataDevice(disp,cantidad))
+        if data['list']:
+            dataValue = base64.b64decode(data['list'][0]['value'])
+            dataTime = data['list'][0]['timestamp']
+            # Limpiar la data y obtener el dispositivo con sus valores
+            device,data = drm.obtenerData(dataValue)
+            if goToFirebase.checkData(dataTime, disp):
+                goToFirebase.send(dataTime,disp,data)
+    time.sleep(60)
