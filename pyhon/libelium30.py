a844a12fec49ca86f39750d7a6917c0fb441a416
sergioeabarcaf <sergioaf1991@gmail.com>
2019-05-25 21:42:41 -0400

    Almacenar las excepciones y el error de conexion en TXT

--- pyhon/libelium30.py
+++ pyhon/libelium30.py
@@ -4,24 +4,28 @@ import json
 import base64
 import time
 import conection
+import timeCustom
+import log
 
 id = ["00:13:A2:00:41:04:37:F1","00:13:A2:00:41:5B:67:EB"]
 
-while (True):
-    if (conection.valid()):
-        print('Conectado')
-        for disp in id:
-            cant = goToFirebase.validDiference30Minutes(disp) * 2
-            # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
-            data = json.loads(drm.getDataDevice(disp,cant))
-            dataReverse = data['list'][::-1]
-            for x in dataReverse:
-                dataValue = base64.b64decode(x['value'])
-                dataTime = x['timestamp']
-                # Limpiar la data y obtener el dispositivo con sus valores
-                device, data = drm.obtenerData(dataValue)
-                if goToFirebase.checkData(dataTime, disp):
-                    goToFirebase.send(dataTime,disp,data)
-    else:
-        print('no hay conexion')
-    time.sleep(60)
+try:
+    while (True):
+        if (conection.valid()):
+            for disp in id:
+                cant = goToFirebase.validDiference30Minutes(disp) * 2
+                # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
+                data = json.loads(drm.getDataDevice(disp,cant))
+                dataReverse = data['list'][::-1]
+                for x in dataReverse:
+                    dataValue = base64.b64decode(x['value'])
+                    dataTime = x['timestamp']
+                    # Limpiar la data y obtener el dispositivo con sus valores
+                    device, data = drm.obtenerData(dataValue)
+                    if goToFirebase.checkData(dataTime, disp):
+                        goToFirebase.send(dataTime,disp,data)
+        else:
+            log.recivedLog(timeCustom.getCurrenDateAndTimeSTR())
+        time.sleep(60)
+except:
+    log.recivedExcept(timeCustom.getCurrenDateAndTimeSTR(), sys.exc_info()[0])
